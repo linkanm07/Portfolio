e9b374ec2f406230bfac6d4e4310b22261bc747a
linkanm07 <benimadhob035@gmail.com>
2026-06-25 23:09:19 +0600

    Implement typing effect for developer titles
    Add typing effect for an array of developer titles.

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 
+const texts = [
+"Full Stack Web Developer",
+"MERN Stack Developer",
+"React Developer",
+"Node.js Developer",
+"Frontend Specialist"
+];
+
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type() {
+if (count === texts.length) {
+count = 0;
+}
+
+currentText = texts[count];
+letter = currentText.slice(0, ++index);
+
+document.getElementById("typing").textContent = letter;
+
+if (letter.length === currentText.length) {
+setTimeout(() => {
+index = 0;
+count++;
+type();
+}, 1500);
+} else {
+setTimeout(type, 80);
+}
+})();
